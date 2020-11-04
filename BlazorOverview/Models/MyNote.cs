687eb28768208151b9738c4d326cc2c0989939d8
Chen-Yu Pao <windperson@gmail.com>
2020-11-04 15:36:20 +0800

    Add project files.

---
+++ BlazorOverview/Models/MyNote.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorOverview.Models
+{
+    public class MyNote : ICloneable
+    {
+        [Required(ErrorMessage ="事項標題不可為空白")]
+        public string Title { get; set; }
+
+        public MyNote Clone()
+        {
+            return ((ICloneable)this).Clone() as MyNote;
+        }
+
+        object ICloneable.Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
